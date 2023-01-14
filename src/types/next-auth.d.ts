8e092599eea27b9304b27833ad611b451176cc68
Fatwa <fatwaaryasatyaakbar@gmail.com>
2023-01-14 17:56:51 +0700

    adding role and adding new model in schema

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,9 +4,18 @@ declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
+  enum Role {
+    ADMIN = "admin",
+    USER = "user",
+    RECEPCIONIST = "receptionist",
+  }
   interface Session {
     user?: {
       id: string;
+      role: string;
     } & DefaultSession["user"];
+  }
+  interface User extends DefaultUser {
+    role: Role;
   }
 }
